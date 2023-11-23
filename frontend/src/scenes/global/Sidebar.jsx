3293b0ee52dc0ccb45a9a73d8f9af63721d732fe
Faheem Akhtar <faheemakhtar19730@gmail.com>
2023-11-23 23:22:13 +0530

    update

--- frontend/src/scenes/global/Sidebar.jsx
+++ frontend/src/scenes/global/Sidebar.jsx
@@ -96,7 +96,7 @@ const Sidebar = () => {
                   alt="profile-user"
                   width="100px"
                   height="100px"
-                  src={`../../assets/profile.png`}
+                  src={`../../assets/Profile.png`}
                   style={{ cursor: "pointer", borderRadius: "50%" }}
                 />
               </Box>
